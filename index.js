0897c249f4a0e090f00e9f432d96f2cdb578ce26
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2023-05-29 13:45:06 +0600

    add new user to db

--- index.js
+++ index.js
@@ -38,6 +38,11 @@ async function run() {
     //users related apis
     app.post('/users', async(req, res)=>{
       const user = req.body;
+      const query = {email: user.email};
+      const existingUser = await usersCollection.findOne(query);
+      if(existingUser){
+        return res.send({message: 'user alredy exists'})
+      }
       const result = await usersCollection.insertOne(user);
       res.send(result)
     })
