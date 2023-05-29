326ad9d7cfceb743f02ec1f8a53a67b757bfe46d
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2023-05-29 21:23:00 +0600

    jwt middleware

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -8,6 +9,24 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json())
+
+const verifyJWT = (req, res, next)=>{
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true, message: 'unouthorized assess'})
+  }
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+    if(err){
+      return res.status(401).send({error: true, message: 'unouthorized assess'})
+    }
+
+    req.decoded = decoded;
+    next();
+  })
+
+}
 
 
 
@@ -33,6 +52,14 @@ async function run() {
     const reviewCollection = client.db("bistroDb").collection('reviews');
     const cartCollection = client.db("bistroDb").collection('carts');
     const usersCollection = client.db("bistroDb").collection('users');
+
+
+    app.post('/jwt', (req, res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+
+      res.send({token})
+    })
 
 
     //users related apis
