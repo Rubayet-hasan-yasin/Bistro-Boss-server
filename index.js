4fee80d1fe102dad0824b3bec7911c4996885254
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2023-06-04 14:45:46 +0600

    admin stats

--- index.js
+++ index.js
@@ -214,6 +214,25 @@ async function run() {
     })
 
 
+    //dashboard home
+    app.get('/admin-stats', verifyJWT, verifyAdmin, async(req, res)=>{
+      const users = await usersCollection.estimatedDocumentCount();
+      const products = await menuCollection.estimatedDocumentCount();
+      const orders = await paymentCollection.estimatedDocumentCount();
+
+      const payments = await paymentCollection.find().toArray();
+      const revenue = payments.reduce((sum, payment)=> sum + payment.price, 0)
+
+      res.send({
+        users,
+        products,
+        orders, 
+        revenue
+      })
+
+    })
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
